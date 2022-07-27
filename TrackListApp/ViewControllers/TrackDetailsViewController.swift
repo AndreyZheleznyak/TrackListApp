bd91f933956f1914d8fc446cf139871475129d9f
AndreyZheleznyak <zheleznyak.dev@gmail.com>
2022-07-27 16:58:10 +0300

    Setup ViewControllers. Folder management setup.

---
+++ TrackListApp/ViewControllers/TrackDetailsViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    
+    @IBOutlet var artCoverImageView: UIImageView!
+    @IBOutlet var trackTitleLabel: UILabel!
+    
+    var track:  Track!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        artCoverImageView.image = UIImage (named: track.title)
+        trackTitleLabel.text = track.title
+    }
+}
